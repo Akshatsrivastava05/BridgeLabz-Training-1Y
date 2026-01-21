b0d7d2e7e2944a11d2c241fd7d94aa0e7119d73d
akshat050606 <akshat050606@gmail.com>
2026-01-21 14:37:59 +0530

    Update Simple_Interest.java

--- basic-programs/Simple_Interest.java
+++ basic-programs/Simple_Interest.java
@@ -4,9 +4,9 @@ class simple_interest
 public static void main(String[]args)
 {
 Scanner sc = new Scanner(System.in);
-System.out.println("Enter principal,rate and time :");
-int t;
+   int t;
 double SI,p,r;
+System.out.println("Enter principal,rate and time :");
 p=sc.nextDouble();
 r=sc.nextDouble();
 t=sc.nextInt();
