07bfbfb6952c9f5c9e7ac518d769d2bba669a408
Akshat <akshat050606@gmail.com>
2026-02-20 23:47:33 +0530

    added solutions to given classes-objects assignment

---
+++ classesAndobjects/src/com/gla/classesAndobjects/classesobjects/level1/Employee.java
@@ -0,0 +1,24 @@
+package com.gla.classesobjects.level1;
+class Employee 
+{
+    String name;
+    int id;
+    double salary;
+    Employee(String n,int i,double s)
+	{
+        name=n;
+        id=i;
+        salary=s;
+    }
+    void display()
+	{
+        System.out.println("Name: "+name);
+        System.out.println("ID: "+id);
+        System.out.println("Salary: "+salary);
+    }
+    public static void main(String args[])
+	{
+        Employee e=new Employee("Akshat",101,500000);
+        e.display();
+    }
+}
